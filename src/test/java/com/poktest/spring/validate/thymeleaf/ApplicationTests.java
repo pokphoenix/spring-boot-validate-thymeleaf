7ada54f3b520156afed609f6aa3f630bd61a92d5
komsan.kr(pok) <pokphoenix@gmail.com>
2019-09-26 10:31:48 +0700

    @add(pok) -add validate ref: https://www.javaguides.net/2018/10/spring-boot-spring-mvc-validating-form.html

---
+++ src/test/java/com/poktest/spring/validate/thymeleaf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poktest.spring.validate.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
